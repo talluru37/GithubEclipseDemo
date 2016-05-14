cd06c661ad0ffd48c668910d2a6ada711c9c20fd
Naveen@MITS <talluru37@gmail.com>
2016-05-14 16:18:46 +0800

    added to github central

--- TestGithub/src/com/mits/sample/TestDemo.java
+++ TestGithub/src/com/mits/sample/TestDemo.java
@@ -5,7 +5,7 @@ public class TestDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("sample added code-2");
+		System.out.println("sample added code-2 22");
 
 	}
 
